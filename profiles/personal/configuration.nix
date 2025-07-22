612e7c3d885ad06693852a231ff2740e7c133363
Zebulon Rouse-Frantzich <zebulonfrantzich@gmail.com>
2025-07-21 21:35:41 -0500

    Makes .sh files executable. Fixes issue where username was not specified on userSettings.

--- profiles/personal/configuration.nix
+++ profiles/personal/configuration.nix
@@ -87,7 +87,7 @@
   # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.${userSettings} = {
+  users.users.${userSettings.username} = {
     isNormalUser = true;
     description = userSettings.name;
     extraGroups = [ "networkmanager" "wheel" "input" "dialout" "video" "render" ];
