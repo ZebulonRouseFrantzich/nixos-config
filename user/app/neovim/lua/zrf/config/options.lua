fe3fb973ff8d91b3290942a8e9df12b87822c96a
Zebulon Rouse-Frantzich <zebulonfrantzich@gmail.com>
2025-08-01 11:15:03 -0500

    Some progress

---
+++ user/app/neovim/lua/zrf/config/options.lua
@@ -0,0 +1,53 @@
+local opt = vim.opt
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+opt.list = true
+opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- Set highlight on search
+opt.hlsearch = true
+
+-- Preview substitutions live, as you type!
+opt.inccommand = "split"
+
+-- Minimal number of screen lines to keep above and below the cursor.
+opt.scrolloff = 10
+
+opt.relativenumber = true
+opt.number = false
+
+-- Enable mouse mode
+opt.mouse = "a"
+
+-- Indents
+-- Tab control
+opt.expandtab = true -- use spaces intead of tabs
+opt.smarttab = true -- tab respects 'tabstop', 'shiftwidth', and 'softtabstop'
+opt.tabstop = 2 -- the visible width of tabs
+opt.softtabstop = 2 -- edit as if the tabs are 4 characters wide
+opt.shiftwidth = 2 -- number of spaces to use for indent and unindent
+opt.shiftround = true -- round indent to a multiple of 'shiftwidth'
+opt.cpoptions:append("I") -- keeps indent when moving around after insert
+
+opt.breakindent = true
+
+-- Save undo history
+opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or capital in search
+opt.ignorecase = true
+opt.smartcase = true
+
+-- Keep signcolumn on by default
+opt.signcolumn = "yes"
+
+-- Decrease update time
+opt.updatetime = 250
+opt.timeoutlen = 300
+
+-- Set completeopt to have a better completion experience
+opt.completeopt = "menu,preview,noselect"
+
+opt.termguicolors = true
--- user/app/neovim/lua/zrf/init.lua
+++ user/app/neovim/lua/zrf/init.lua
@@ -1,3 +1,5 @@
 -- NOTE: The keymaps need to be loaded first because the leader settings
 -- have to be set before plugins are loaded.
 require("zrf.config.keymaps")
+require("zrf.config.options")
+require("zrf.colors")
